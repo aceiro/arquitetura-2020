4d3f0fc5d1b3c65f5fbaf0aaaa52b0a29d08b4de
Erik Aceiro Antonio <aceiro@gmail.com>
2020-03-30 20:49:22 -0300

    Added new comments

--- 0-principles/Main.java
+++ 0-principles/Main.java
@@ -1,19 +1,31 @@
-abstract class Veiculo{
-  int qtdPortas;
+abstract class Veiculo{             // #1. SRP - Single Responsability Principle
+  private int qtdPortas = 4;        // o objeto veiculo é responsável por ele mesmo, 
+                                    // ou seja, ele define um Veiculo e nada mais
+                                    
+                                    //#2. OCP - Encapsulamento proteje o objeto e seus atributos
   abstract void exibePortas();
+  // abstract void exibePessoa(); - #1. SRP - perde a coesão
+  public void setPortas(int qtdPortas){
+    this.qtdPortas = qtdPortas;
+  }
+  public int getPortas(){ 
+    return this.qtdPortas;
+  }
 }
 
 class Carro extends Veiculo{
+
   void exibePortas(){
-      qtdPortas = 4;
-      System.out.println("Carro::qtdPortas = " + qtdPortas);
+      this.setPortas(4);
+      System.out.println("Carro::qtdPortas = " + this.getPortas());
   }
 }
 
 class Moto extends Veiculo{
   void exibePortas(){
-      qtdPortas = 0;
-      System.out.println("Moto::qtdPortas = " + qtdPortas);
+      // quebra o encapsulamento, viola o princípio #2. OCP - Open-Closed Principle       
+      // qtdPortas = 10;
+      // System.out.println("Moto::qtdPortas = " + qtdPortas);
   }
 
   void alteraMarcha(){
@@ -26,7 +38,7 @@ public class Main{
     // Vetor Polimorfico
     // pode ser de várias formas a partir da classe
     // base - Veiculo, no caso, Moto e Carro
-    Veiculo v[] = { new Moto(), new Moto(), new Carro() };
+    Veiculo v[] = { new Carro(), new Moto(), new Moto() };
     
     for (Veiculo veiculo : v) { // LSP
       veiculo.exibePortas();
